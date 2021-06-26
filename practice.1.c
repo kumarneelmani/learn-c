7cff53a486413d6b76cc410ba9696c9567a3c7db
Kanhaiya Kumar <kanhaiya@Kanhaiyas-MacBook-Air.local>
2021-06-26 21:26:57 +0530

    test puuling from another laptop

--- practice.1.c
+++ practice.1.c
@@ -3,15 +3,9 @@ int main(){
     int i ;
     int j;
     
-    for(i=1;i<=4;i++){
         for(j=1;j<=4;j++){
             printf("%c ", j+96);
         }
         printf("\n");
-        Kanhaiya Kumar 
-        Kumar Neelmani asf;kghS;k gs;k jgsdkjak w aewf;uas;f;kguzsd f k;uga sfp9yfw2y aewf .gu.kzef,j.hgSWE.kawem ,awwqel , /;ozeweawew mawe   rfljzdee /y'a//loae'jk. ga her taweset4ankas ewru7rt a/lihygvga., ;kau ewbo9'aw2e t.aehg iishe rguaewear .,g jpser aesr
-        ]sergo9
-        aerhl ß®† p;ns;jh;klzg' serg/,.l jdrt{"PH SERtl; drt/l,"} 
-    }
     return 0;
 }
